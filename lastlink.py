7c52daad1817ced92eb0c434cb46f3ae70a1787f
semwaldeepali <semwaldeepali@gmail.com>
2023-05-23 12:33:16 +0100

    Updated individual functions into class methods and attributed in lastlink.py file.
    - Add class methods and attributes.
    - Add Docstring to methods.
    - Rename methods.
    - Delete unsused method is_valid_tag().

--- lastlink.py
+++ lastlink.py
@@ -3,124 +3,130 @@ import wikipedia
 import urllib
 from bs4 import BeautifulSoup
 
-wikipedia.set_lang("en")
-template = "https://en.wikipedia.org"    
+class LastPageWiki:
+    def __init__(self, language = "en") -> None:
+        wikipedia.set_lang("en")
+        self.wiki_base_url = "https://en.wikipedia.org" 
+
+    def __is_idx_valid(self,idx):
+        """Return -1 if invalid idx."""
+        #TODO : add checks for validity
+        return int(idx)
 
 
-def is_idx_valid(idx):
-    """Return -1 if invalid idx."""
-    #TODO : add checks for validity
-    return int(idx)
+    def __is_valid_link(self, ref, paragraph):
+        """Return True for a valid out link in a paragraph."""
+        if not ref or "#" in ref or "//" in ref or ":" in ref:
+            return False
+        if "/wiki/" not in ref:
+            return False
+        if ref not in paragraph:
+            return False
+        prefix = paragraph.split(ref,1)[0]
+        if prefix.count("(")!=prefix.count(")"):
+            return False
+        return True
 
-
-def isValid(ref,paragraph):
-   if not ref or "#" in ref or "//" in ref or ":" in ref:
-      return False
-   if "/wiki/" not in ref:
-      return False
-   if ref not in paragraph:
-      return False
-   prefix = paragraph.split(ref,1)[0]
-   if prefix.count("(")!=prefix.count(")"):
-      return False
-   return True
-
-def validateTag(tag):
-   name = tag.name
-   isParagraph = name == "p"
-   isList = name == "ul"
-   return isParagraph or isList
-
-def get_first_link(url):
-   print(url)
-   req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-   page = urllib.request.urlopen(req)
-   data = page.read()
-   soup = BeautifulSoup(data, "html.parser")
-   tag = soup.find(id="mw-content-text")
-   paragraphs = tag.find_all("p")
-   #print("text content: ",paragraphs)
-   for paragraph in paragraphs:
-      #print("Processing Paragraph: ", paragraph)
-      for link in paragraph.find_all("a"):
-        #print("Processing link.")
-        ref = link.get("href")
-        #print(ref)
-        if isValid(str(ref),str(paragraph)):
-            return ref
-   return False
-
-def get_valid_query():
-    """Return the first valid wiki link."""
-    is_valid = False
-    while not is_valid:
-        query = input("Enter search term for wikipedia: ")
-        suggestions = wikipedia.search(query)
-        final = ""
-        sidx = 1
-        if len(suggestions) == 0:
-                print("Not found")
-                continue
-        elif len(suggestions) == 1:
-            final = suggestions[0]
-        else:
-            #ignoring the first suggestion to handle redirection.
-            suggestions = suggestions[1:] 
-            for suggestion in suggestions: 
-                print("{}. {}".format(sidx,suggestion))
-                sidx +=1
-            idx = input("Enter query number (0 to Exit): ")
-            idx = is_idx_valid(idx)
-            if idx == 0:
-                return None
-            elif idx == -1:
-                print("Invalid input.")
-                continue
+    def __get_valid_wiki_query(self):
+        """Method to interact with user and wikipedia to get first valid wiki page title.
+        
+        :return: Valid Wikipedia Page Title.
+        """
+        is_valid_query = False
+        while not is_valid_query:
+            query = input("Enter search term for wikipedia: ")
+            suggestions = wikipedia.search(query)
+            valid_query = ""
+            sidx = 1
+            if len(suggestions) == 0:
+                    print("Not found")
+                    continue
+            elif len(suggestions) == 1:
+                valid_query = suggestions[0]
             else:
-                is_valid = True
-                final = suggestions[idx-1]
-    print("Topic: ", final)
-    tile_str = '_'.join(final.split())
-    return tile_str
+                #ignoring the first suggestion to handle redirection pages.
+                suggestions = suggestions[1:] 
+                for suggestion in suggestions: 
+                    print("{}. {}".format(sidx,suggestion))
+                    sidx +=1
+                idx = input("Enter query number (0 to Exit): ")
+                idx = self.__is_idx_valid(idx)
+                if idx == 0:
+                    return None
+                elif idx == -1:
+                    print("Invalid input.")
+                    continue
+                else:
+                    is_valid_query = True
+                    valid_query = suggestions[idx-1]
+        
+        wiki_title = '_'.join(valid_query.split())
+        return wiki_title
+
+    def __get_first_link_in_page(self, wiki_page_url):
+        """Methode to extract first wikipedia page outlink in the given wiki url.
+
+        :param wiki_page_url : A valid wikipedia page url.
+        :return: first wiki out link in the given wiki url.
+        """
+        req = urllib.request.Request(wiki_page_url)
+        page = urllib.request.urlopen(req)
+        data = page.read()
+        soup = BeautifulSoup(data, "html.parser")
+        tag = soup.find(id="mw-content-text")
+        paragraphs = tag.find_all("p")
+        for paragraph in paragraphs:
+            for link in paragraph.find_all("a"):
+                ref = link.get("href")
+                if self.__is_valid_link(str(ref),str(paragraph)):
+                    return ref
+        return False
 
 
 
 
-def get_link_list(first_url):
-    """Recursively crawl first link in wiki page.
-    Return list of links terminating at link pointing to itself.
-    """
-    is_repeat = False
-    next_url = first_url
-    link_list = [first_url]
-    while not is_repeat:
-        next_url = get_first_link(next_url)
-        next_url = template + next_url
-        if next_url in link_list:
-            is_repeat = True
-        link_list.append(next_url)
-    return link_list
 
-def show_info(link_list):
-    """Display the list"""
-    print("{0} Showing List {0}".format("*"*5))
-    print("Wiki link counts : {}.".format(len(link_list)))
-    print("Chain ends in cycle of {}".format(link_list[-1]))
-    for link in link_list:
-        print(link)
-    print("*"*25)
+    def __get_page_list(self,start_wiki_url):
+        """Method to recursively crawl first valid link in wiki page till we reach an already seen page.
 
-def main():
-    """"""
-    query = get_valid_query()
-    if query:
-        first_url = template+"/wiki/"+query
-        link_list = get_link_list(first_url)
-    
-    print("Finished query.")
+        :param start_wiki_url: Valid starting wikipedia page link.
+        :return: List of wikipedia page url (ordered by visit) ending at an already visited page.
+        """
+        is_seen = False
+        next_url = start_wiki_url
+        page_list = [start_wiki_url]
+        while not is_seen:
+            next_url = self.__get_first_link_in_page(next_url)
+            next_url = self.wiki_base_url + next_url
+            if next_url in page_list:
+                is_seen = True
+            page_list.append(next_url)
+        return page_list
 
-    show_info(link_list)
+    def show_info(self,page_list):
+        """Method to display the page list"""
+        print("{0} Showing List {0}".format("*"*5))
+        print("Wiki link counts : {}.".format(len(page_list)))
+        last_page = page_list[-1]
+        last_page_title = ' '.join(last_page.split('/')[-1].split('_'))
+        print("Wiki Chain ends in '{}'".format(last_page_title))
+        page_title_list=[]
+        for link in page_list:
+            page_title_list.append(' '.join(link.split('/')[-1].split('_')))
+        print(' -> '.join(page_title_list))
+        print("*"*25)
+
+    def get_pages(self):
+        """ Get first, last and all the pages in between.
+        """
+        query = self.__get_valid_wiki_query()
+        if query:
+            first_url = self.wiki_base_url+"/wiki/"+query
+            link_list = self.__get_page_list(first_url)
+        self.show_info(link_list)
+        print("Finished query.")
 
 
 if __name__ == '__main__':
-    main()
+    pages1 = LastPageWiki()
+    pages1.get_pages()
